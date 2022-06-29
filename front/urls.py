419a73b8df19bee4dccd460c5c95ce3e8074f3b6
mwizard9 <manishhimalaya64@gmail.com>
2022-06-29 21:34:43 +0545

    error

--- front/urls.py
+++ front/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('',views.frontpage,name="frontpage")
+    path('',views.frontpage,name="frontpage"),
+    path('register',views.register,name="register"),
+    path('login',views.login,name="login"),
+    path('logout',views.logout,name="logout")
 ]
