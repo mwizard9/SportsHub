fceeb2fcde625caf5714438dfe17d081a0af4472
mwizard9 <manishhimalaya64@gmail.com>
2022-06-29 17:48:38 +0545

    database

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sports',
+        'USER': 'postgres',
+        'PASSWORD': 'maxwell100',
+        'HOST': 'localhost'
     }
 }
 
