bf2c0fd5f70f7839c7506fa4905bc8dfbf8c1768
mwizard9 <manishhimalaya64@gmail.com>
2022-06-29 18:56:00 +0545

    making_dynamic

--- sportshub/admin.py
+++ sportshub/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from.models import sport
 
 # Register your models here.
+admin.site.register(sport)
--- sportshub/views.py
+++ sportshub/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from.models import sport
 
 # Create your views here.
 def front(request):
-    return render(request,'home.html')
+    sports = sport.objects.all()
+
+    return render(request,'home.html',{'sports':sports})
