7020dfd9c2657d7c09e94b8cad66953b80976538
mwizard9 <manishhimalaya64@gmail.com>
2022-07-03 21:43:32 +0545

    login done

--- front/views.py
+++ front/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
+from sportshub.views import front
+
 
 # Create your views here.
 def frontpage(request):
@@ -17,7 +19,7 @@ def login(request):
 
          if user is not None:
             auth.login(request,user)
-            return redirect('home')
+            return redirect(front)
          else:
             messages.info(request,"invalid username password")
             return redirect('login')
