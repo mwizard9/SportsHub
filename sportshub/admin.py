02ac57168f4dbe6f85622c7eb5eb98d1ad358a83
mwizard9 <manishhimalaya64@gmail.com>
2022-07-06 16:41:39 +0545

    add admin

--- sportshub/admin.py
+++ sportshub/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from.models import Trend, sport
+from.models import Trend, Nepal, sport,kid
 
 # Register your models here.
 admin.site.register(sport)
 admin.site.register(Trend)
+admin.site.register(Nepal)
+admin.site.register(kid)
