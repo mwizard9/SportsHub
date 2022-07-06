2350daf137e129462e0c2c79072d8533ec2b5f65
mwizard9 <manishhimalaya64@gmail.com>
2022-07-06 16:42:59 +0545

    makin urls

--- sportshub/urls.py
+++ sportshub/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('',views.front,name="front")
+    path('',views.front,name="front"),
+    path('nepal',views.nepal,name="nepal"),
+     path('Kids',views.Kids,name="Kids")
+    
+
 ]
