6a21e290f498e7058929d5c3900f926b1b3ade4a
mwizard9 <manishhimalaya64@gmail.com>
2022-07-06 16:43:40 +0545

    adding views

--- sportshub/views.py
+++ sportshub/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from.models import sport
 from.models import Trend
+from.models import Nepal
+from.models import kid
 
 # Create your views here.
      
@@ -9,5 +11,16 @@ from.models import Trend
 def front(request):
     sports = sport.objects.all()
     Trends = Trend.objects.all()
+  
 
     return render(request,'home.html',{'sports':sports ,'Trends':Trends})
+
+def nepal(request):
+    nepals = Nepal.objects.all()
+
+    return render(request,'nepal.html',{'nepals':nepals})
+
+
+def Kids(request):
+    kids = kid.objects.all()
+    return render(request,'Kids.html',{'kids':kids})
