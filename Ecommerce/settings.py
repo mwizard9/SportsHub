c9aca85bebf6d1e429697b70b3146374c5226194
mwizard9 <manishhimalaya64@gmail.com>
2022-07-13 19:34:25 +0545

    cart_app

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from importlib_metadata import files
 
+import cart
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartCOnfig',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+cart.SESSION_ID= 'cart'
