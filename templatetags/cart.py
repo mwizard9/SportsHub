9b8746031c430e29569376a6fc59f6c9016a9a39
mwizard9 <manishhimalaya64@gmail.com>
2022-07-17 14:07:10 +0545

    completefilter

--- templatetags/cart.py
+++ templatetags/cart.py
@@ -1,11 +1,43 @@
 from django import template
 
+from sportshub.models import sport
+
 
 register = template.Library()
 
 @register.filter(name='is_in_cart')
-def is_in_cart(product ):
-    #keys = cart.keys()
-    #print(keys)
-    print(product)
-    return True;
+def is_in_cart(sport , cart):
+    keys = cart.keys()
+    for id in keys:
+
+        if int(id) == sport.id:
+            return True
+
+    return False
+
+@register.filter(name='cart_quantity')
+def cart_quantity(product  , cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            return cart.get(id)
+    return 0;
+
+
+@register.filter(name='price_total')
+def price_total(product  , cart):
+    return product.price * cart_quantity(product , cart)
+
+
+@register.filter(name='total_cart_price')
+def total_cart_price(products , cart):
+    sum = 0 ;
+    for p in products:
+        sum += price_total(p , cart)
+
+    return sum
+            
+
+    
+    
+   
