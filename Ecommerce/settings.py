1d2949db98e8aa99dd5297405510b2c24043e6cd
mwizard9 <manishhimalaya64@gmail.com>
2022-07-17 11:24:35 +0545

    finally filter

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart',
     
 ]
 
@@ -67,6 +68,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries':{
+                'cart': 'templatetags.cart',
+            
+            }
         },
     },
 ]
