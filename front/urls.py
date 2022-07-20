042b0baf107fb72df24a6f499bab5350f2a22b62
mwizard9 <manishhimalaya64@gmail.com>
2022-07-20 16:44:24 +0545

    removing some content

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+
+from sportshub.views import front
 from.import views
+
 
 urlpatterns = [
     path('',views.frontpage,name="frontpage"),
